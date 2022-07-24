9c99812ed063a61b4e12a99a4f35aecf89682f73
arthamevia <yuli45904@gmail.com>
2022-07-24 19:06:30 +0700

    Projek pkl minggu ke 2

--- resources/views/tugas/edit.blade.php
+++ resources/views/tugas/edit.blade.php
@@ -7,7 +7,7 @@
                 @include('layouts/_flash')
                 <div class="card">
                     <div class="card-header">
-                        Data Siswa
+                        Data siswa
                     </div>
                     <div class="card-body">
                         <form action="{{ route('tugas.update', $tugas->id) }}" method="post">
