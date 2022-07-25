00bbe7fa377c244936a8b888117871565b4e163a
arthamevia <yuli45904@gmail.com>
2022-07-25 18:26:00 +0700

    Relasi

--- database/migrations/2022_07_25_023334_create_walis_table.php
+++ database/migrations/2022_07_25_023334_create_walis_table.php
@@ -20,7 +20,7 @@ class CreateWalisTable extends Migration
             $table->unsignedBigInteger('id_siswa');
             // membuat fk id_siswa yang mengacu kpd field id di table
             // siswas
-            $table->foreign('id_siswa')->references('id')->on('siswas')
+            $table->foreign('id_siswa')->references('id')->on('tugas')
                 ->onDelete('cascade');
             $table->timestamps();
         });
